5abeded953aed480a4e75eb817b0fab156fa2521
Hector <contact@salaxer.com>
2022-06-29 00:28:20 -0500

    [fixed v2]: return NotFound if the product doesn't exist solve this error

--- pages/menu/[id].tsx
+++ pages/menu/[id].tsx
@@ -17,7 +17,9 @@ interface ProductProps{
 }
 
 const MenuItem = ({ product, err }:ProductProps) => {
-  
+  if (err || !product) {
+      return <NotFound/>
+  }
   return (
     <div >
       <Head>
@@ -40,49 +42,45 @@ const MenuItem = ({ product, err }:ProductProps) => {
         <link rel="apple-touch-icon" href={product.image}/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       </Head>
-      {(!err && product) ?
-        <main className='bg-blue-100'>
-          <AllScreen>
-            <section className={styles.screenShowMenu}>
-              <div className='relative'>
-                <div className={styles.containerIMG}>
-                  <PreviewImage>
-                    <Image src={product.image} alt='restaurant' layout='fill' objectFit='cover' className={styles.imageDish}></Image>
-                  </PreviewImage>
+      <main className='bg-blue-100'>
+        <AllScreen>
+          <section className={styles.screenShowMenu}>
+            <div className='relative'>
+              <div className={styles.containerIMG}>
+                <PreviewImage>
+                  <Image src={product.image} alt='restaurant' layout='fill' objectFit='cover' className={styles.imageDish}></Image>
+                </PreviewImage>
+              </div>
+            </div>
+            <div className={styles.descriptionShowMenu}>
+              <span className={styles.availability}>
+                { product.available ? <Tag severity='success' shadow='lg' size='4xl' value="Disponible"></Tag> : <Tag shadow='lg' severity='danger' size='4xl' value="Agotado"></Tag>}
+                { product.discount > 0 && <Tag className='mt-5' shadow='lg' severity='warning' size='3xl' value={`-${product.discount}% OFF`}></Tag>}
+              </span>
+              <h1 className='text-center'>{product.title}</h1>
+              <div className='flex gap-5'>
+                { product.discount > 0 ? 
+                  <>
+                    <span className={styles.lastPrice}>${product.price}</span>
+                    <h2 style={{color: 'var(--maincolorgreen)'}} className='text-5xl font-semibold'>${discount(product.price, product.discount)}</h2>
+                  </>
+                  :
+                  <h2 className='text-5xl font-semibold'>${product.price}</h2>
+                }
+                <div className={styles.heart}>
+                  <i className='pi pi-heart'></i>
+                  <ToolTip position='bottom' value='Agregar a favoritos'></ToolTip>
                 </div>
               </div>
-              <div className={styles.descriptionShowMenu}>
-                <span className={styles.availability}>
-                  { product.available ? <Tag severity='success' shadow='lg' size='4xl' value="Disponible"></Tag> : <Tag shadow='lg' severity='danger' size='4xl' value="Agotado"></Tag>}
-                  { product.discount > 0 && <Tag className='mt-5' shadow='lg' severity='warning' size='3xl' value={`-${product.discount}% OFF`}></Tag>}
-                </span>
-                <h1 className='text-center'>{product.title}</h1>
-                <div className='flex gap-5'>
-                  { product.discount > 0 ? 
-                    <>
-                      <span className={styles.lastPrice}>${product.price}</span>
-                      <h2 style={{color: 'var(--maincolorgreen)'}} className='text-5xl font-semibold'>${discount(product.price, product.discount)}</h2>
-                    </>
-                    :
-                    <h2 className='text-5xl font-semibold'>${product.price}</h2>
-                  }
-                  <div className={styles.heart}>
-                    <i className='pi pi-heart'></i>
-                    <ToolTip position='bottom' value='Agregar a favoritos'></ToolTip>
-                  </div>
-                </div>
-                <h5 className='text-3xl text-center mt-10'>{product.details}</h5>
-                <ol style={{listStyle: "outside"}}>
-                  {product.time > 0 && <li className='text-2xl mt-3'>Preparacion en {product.time} minutos <i style={{color: 'var(--maincolorgreen)'}} className='pi pi-check-circle'></i></li>}
-                  <li className='text-2xl mt-3'>con un tamaño de {product.size}   <i style={{color: 'var(--maincolorgreen)'}} className='pi pi-check-circle'></i></li>
-                </ol>
-              </div>
-            </section>
-          </AllScreen>
-        </main>
-        :
-      <NotFound/>
-      }
+              <h5 className='text-3xl text-center mt-10'>{product.details}</h5>
+              <ol style={{listStyle: "outside"}}>
+                {product.time > 0 && <li className='text-2xl mt-3'>Preparacion en {product.time} minutos <i style={{color: 'var(--maincolorgreen)'}} className='pi pi-check-circle'></i></li>}
+                <li className='text-2xl mt-3'>con un tamaño de {product.size}   <i style={{color: 'var(--maincolorgreen)'}} className='pi pi-check-circle'></i></li>
+              </ol>
+            </div>
+          </section>
+        </AllScreen>
+      </main>
     </div>
   )
 }
